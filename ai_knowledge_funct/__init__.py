455d4466437d13f839e3bc378ea1c59b6f0761d8
Idea Bosque <ideabosque@gmail.com>
2025-01-09 22:45:51 -0800

    Initial checkin.

---
+++ ai_knowledge_funct/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+__author__ = "bibow"
+
+__all__ = ["ai_knowledge_funct"]
+from .ai_knowledge_funct import AIKnowledgeFunct
---
+++ ai_knowledge_funct/ai_knowledge_funct.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import humps
+
+__author__ = "bibow"
+
+import logging
+import traceback
+from typing import Any, Dict, List
+
+from openai_funct_base import OpenAIFunctBase
+
+
+class AIKnowledgeFunct(OpenAIFunctBase):
+
+    def __init__(self, logger: logging.Logger, **setting: Dict[str, Any]):
+        try:
+            self.logger = logger
+            OpenAIFunctBase.__init__(self, logger, **setting)
+        except Exception as e:
+            log = traceback.format_exc()
+            self.logger.error(log)
+            raise e
+
+    def get_knowledge_rag(self, **arguments: Dict[str, Any]) -> List[Dict[str, Any]]:
+        try:
+            self.logger.info(f"Arguments: {arguments}")
+
+            variables = {
+                "userQuery": arguments["user_query"],
+                "indexName": self.setting["index_name"],
+                "documentSource": self.setting["document_source"],
+            }
+            results = self.execute_graphql_query(
+                "ai_knowledge_graphql", "knowledgeRag", "Query", variables
+            )["knowledgeRag"]["results"]
+
+            return humps.decamelize(results)
+        except Exception as e:
+            log = traceback.format_exc()
+            self.logger.error(log)
+            raise e
